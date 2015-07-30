77a0e2b919505e1e21af63aa49bc0e3bab284cf1
Daniel Gading <dgading@gmail.com>
2015-07-30 19:21:08 -0400

    improves basic game logic

--- js/main.js
+++ js/main.js
@@ -1,33 +1,44 @@
 var robotInit = 0;
 var guess = document.getElementById('submitGuess');
-var word = "apple".split("");
+var word = "puppy";
+var guessedLetters = [];
+var missedLetters = [];
 
-console.log(word);
+
+// console.log(word);
 var newGuess = function() {
+  var yourGuess = false;
   var newLetter = document.getElementById('yourGuess').value;
+  console.log(newLetter);
   if (newLetter.length == 0) {
     alert("You gotta add a letter!");
-  } else if (newLetter.length > 0) {
-    for (var i = 0; i < word.length; i++) {
-      // compare newLetter to each indice in the word
-      console.log(word[i]);
-      if (newLetter == word[i]) {
-        document.getElementById('goodGuess').innerHTML += newLetter;
-      } else {
-        document.getElementById('badGuess').innerHTML += newLetter;
-
+  } else {
+    console.log(guessedLetters.indexOf(newLetter));
+    if (guessedLetters.indexOf(newLetter) >= 0) {
+        alert("You've already guessed that letter!");
+    } else {
+      guessedLetters.push(newLetter);
+      for (var i = 0; i < word.length; i++) { //compares guess to each letter in word
+        if (newLetter === word[i]) {
+          document.getElementById('goodGuess').innerHTML += word[i];
+          console.log(guessedLetters);
+          yourGuess = true;
+        }
+      }
+        if (yourGuess === false) {
+          robotInit++;
+          document.getElementById('robotCounter').innerHTML = robotInit;
+          guessedLetters.push(newLetter);
+          missedLetters.push(newLetter);
+        }
+        if(robotInit == 5) {
+          alert("Game over!");
+        }
       }
     }
-
-  } else {
-    robotInit++;
-    document.getElementById('robotCounter').innerHTML = robotInit;
-    if(robotInit == 5) {
-      alert("Game over!");
-    }
-  }
   document.getElementById('yourGuess').value = "";
-  console.log(word[3]);
+  document.getElementById('badGuess').innerHTML = missedLetters.toString();
+
 };
 
 guess.addEventListener('click', newGuess, true);
